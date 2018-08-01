7f4660a7eb78fe429051c87e5d159560a889b295
Christopher Weber <chrisrrweber@gmail.com>
2018-08-01 00:00:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string);
+  return string.toUpperCase();
+}
+
+function logWhisper(string) {
+  console.log(string);
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase);
+  return "I can't hear you!"
+}
+
+function sayHiToGrandma(string) {
+  
+}
